22c56cad47a1bd7bd5390c57316cd7e97ff30428
Stephan <Stephan.caldas@gmail.com>
2025-02-23 12:08:44 -0300

    Primeiro commit

---
+++ src/Contador.java
@@ -0,0 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o primeiro número:");
+        int parametroUm = scanner.nextInt();
+        System.out.println("Digite o segundo número:");
+        int parametroDois = scanner.nextInt();
+
+        try {
+            contar (parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException e) {
+
+            System.out.println("O segundo número tem que ser maior que o primeiro.");
+        }
+
+        scanner.close();
+    }
+
+    static void contar (int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int indice = 0; indice < contagem; indice++) {
+            System.out.println("Número: " + (indice + 1));
+        }
+    }
+}
